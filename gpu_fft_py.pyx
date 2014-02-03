9535ca35abe82f384bf3ce6a8cc403fa2612016e
Tom Ritchford <tom@swirly.com>
2014-02-03 13:59:29 -0500

    Improve clean target in build;  start to create C interface in pyx file.

--- gpu_fft_py.pyx
+++ gpu_fft_py.pyx
@@ -1,2 +1,24 @@
-def test():
-   print('pass')
+cdef extern from "mailbox.h":
+  int mbox_open()
+  mbox_close(int mb)
+
+
+cdef extern from "gpu_fft.h":
+  cdef struct GPU_FFT_COMPLEX:
+    float re, im
+
+  cdef struct GPU_FFT:
+    GPU_FFT_COMPLEX* input
+    GPU_FFT_COMPLEX* output
+    int mb, step
+    unsigned timeout, noflush, handle, size, vc_msg
+
+  int gpu_fft_prepare(
+      int mb,         # mailbox file_desc
+      int log2_N,     # log2(FFT_length) = 8...17
+      int direction,  # GPU_FFT_FWD: fft(); GPU_FFT_REV: ifft()
+      int jobs,       # number of transforms in batch
+      GPU_FFT **fft)
+
+  unsigned gpu_fft_execute(GPU_FFT *info)
+  void gpu_fft_release(GPU_FFT *info)
--- setup.py
+++ setup.py
@@ -1,20 +1,34 @@
+import subprocess
+import sys
+
 from Cython.Distutils import build_ext
 from distutils.core import setup
 from distutils.extension import Extension
 
+if 'clean' in sys.argv:
+  print 'Deleting cython files...'
+  subprocess.Popen('rm -rf build', shell=True, executable='/bin/bash')
+  subprocess.Popen('rm -rf *.c', shell=True, executable='/bin/bash')
+  subprocess.Popen('rm -rf *.so', shell=True, executable='/bin/bash')
+
 setup(
   cmdclass = {'build_ext': build_ext},
   ext_modules = [
-    Extension("gpu_fft_py",
-              sources=["gpu_fft_py.pyx",
-                       "gpu_fft/gpu_fft.c",
-                       "gpu_fft/gpu_fft_shaders.c",
-                       "gpu_fft/gpu_fft_twiddles.c",
-                       "gpu_fft/mailbox.c",
-                       ],
-              extra_compile_args=["-Igpu_fft"],
-              extra_link_args=["-lrt", "-lm"],
-              )
+    Extension(
+      'gpu_fft_py',
+
+      sources=[
+        'gpu_fft_py.pyx',
+        'gpu_fft/gpu_fft.c',
+        'gpu_fft/gpu_fft_shaders.c',
+        'gpu_fft/gpu_fft_twiddles.c',
+        'gpu_fft/mailbox.c',
+        ],
+
+      extra_compile_args=['-Igpu_fft'],
+
+      extra_link_args=['-lrt', '-lm'],
+      )
     ]
 )
 
