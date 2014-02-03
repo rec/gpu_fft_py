f37e66d3744863a51ea9b10fa47463fbfbc824de
Tom Ritchford <tom@swirly.com>
2014-02-03 13:15:38 -0500

    Starting to bring in the library files.

---
+++ gpu_fft_py.pyx
@@ -0,0 +1,2 @@
+def test():
+   print('pass')
--- setup.py
+++ setup.py
@@ -1,6 +1,23 @@
+from Cython.Distutils import build_ext
 from distutils.core import setup
-from Cython.Build import cythonize
+from distutils.extension import Extension
 
 setup(
-  ext_modules = cythonize("gpu_fft.pyx")
+  cmdclass = {'build_ext': build_ext},
+  ext_modules = [
+    Extension("gpu_fft_py",
+              sources=["gpu_fft_py.pyx",
+                       "gpu_fft/gpu_fft.c",
+                       "gpu_fft/gpu_fft_shaders.c",
+                       "gpu_fft/gpu_fft_twiddles.c",
+                       "gpu_fft/mailbox.c",
+                       ],
+              extra_compile_args=["-Igpu_fft"],
+              extra_link_args=["-lrt", "-lm"],
+              )
+    ]
 )
+
+
+#  ext_modules = cythonize('gpu_fft.pyx',
+#                          extra_link_args='-Lgpu_fft -lgpufft')
